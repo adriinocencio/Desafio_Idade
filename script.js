fac2c42435cce208f6c106b3ef750e9fac9dc98a
Adrielly Inocencio <adrielly.inoccencio@gmail.com>
2023-05-04 15:15:29 -0300

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function idade(){
+
+    var nome = document.getElementById('nomedapessoa').value;
+    var idade =document.getElementById('idadedapessoa').value;
+    console.log(nome);
+    console.log(idade);
+
+    if(idade <= 11) {
+        document.getElementById('resultado').innerText = "Olá " + nome + " essa é a idade de uma criança";
+    } else if (idade >=12 && idade <=20) {
+        document.getElementById('resultado').innerText = "Olá " + nome + " essa é a idade de um Adolescente"; 
+    } else if (idade >=21 && idade <=65) {
+        document.getElementById('resultado').innerText = "Olá " + nome + " essa é a idade de um Adulto"; 
+    } else if (idade >= 65) {
+        document.getElementById('resultado').innerText = "Olá " + nome + " essa é a idade de um Idoso " + idade + " anos de idade"; 
+    }
+}
